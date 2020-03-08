c77f51c85f86dfa9774c878447190d1e5bf1c073
Arjen Bokhoven <arjen@high5.nl>
2020-03-08 20:46:52 +0800

    Rems are good

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync");
 
-gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
-});
-
-//Watch task
-gulp.task('watch',function() {
-  gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
-});
+// Compile into CSS
+function style() {
+  // 1 where is my scss file?
+  return (
+    gulp
+      .src("app/scss/**/*.scss")
+      // 2 pass that file through sass compiler
+      .pipe(sass().on('error', sass.logError))
+      // 3 where do i save the compiled css?
+      .pipe(gulp.dest("app/css"))
+      // 4 stream changes to all browsers
+      .pipe(browserSync.stream())
+  );
+}
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./app"
+    }
+  });
+  gulp.watch("app/scss/**/*.scss", style);
+  gulp.watch("app/*.html").on("change", browserSync.reload);
+  gulp.watch("app/js/**/*.js").on("change", browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
