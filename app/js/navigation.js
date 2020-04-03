664f0682852ab3fa777a5481bae250f53609058c
Arjen Bokhoven <arjen@high5.nl>
2020-04-03 10:13:03 +0800

    Simpler navigation.

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,22 +1,8 @@
-;(function($) {
-  $(function() {
-    $('nav ul li > a:not(:only-child)').click(function(e) {
-      $(this)
-      .siblings('.nav-dropdown')
-      .toggle()
-      $('.nav-dropdown')
-      .not($(this).siblings())
-      .hide()
-      e.stopPropagation()
-    })
-    $('html').click(function() {
-      $('.nav-dropdown').hide()
-    })
-    $('#nav-toggle').on('click', function() {
-      this.classList.toggle('active')
-    })
-    $('#nav-toggle').click(function() {
-      $('nav ul').toggle()
-    })
-  })
-})(jQuery)
+function openDrawerMenu(){
+  var x = document.getElementById("mainNavBar");
+  if (x.className === "navBar"){
+    x.className += " responsive";
+  } else {
+    x.className = "navBar";
+  }
+}
